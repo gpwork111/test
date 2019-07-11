3ce56ba39e26ebe5971644e94fc950a9e86a3e9f
guopeng <gpwork111@163.com>
2019-07-11 14:26:41 +0800

    spring security test

--- src/main/java/com/crcc/test/TestApplication.java
+++ src/main/java/com/crcc/test/TestApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication{
 
-    char aChar = 'a';
+    char aChar = 'b';
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
